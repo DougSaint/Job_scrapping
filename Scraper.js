4a0a8dc95e28685ba96d333bc3075175c5f6a51a
Douglas Santos <ds4208151@gmail.com>
2023-09-04 01:39:09 -0300

    fix: request

--- Scraper.js
+++ Scraper.js
@@ -9,7 +9,7 @@ class Scraper {
   currentlyScrapingGoogle = false;
 
   googleScraper = async () => {
-    const googleJobsUrl = `https://www.google.com.br/search?q=(desenvolvedor+|+programador)+(front-end+|+front+end)+remoto+contratando+|+procurando+brasil&sca_esv=562432527&source=hp&ei=s1f1ZOWYOb6A9u8PlZGByAU&iflsig=AD69kcEAAAAAZPVlw3Ywn8w1JvWomVnny-qr_2Ars_vD&uact=5&oq=(desenvolvedor+%7C+programador)+(front-end+%7C+front+end)+remoto+contratando+%7C+procurando+%7C+brasil&gs_lp=Egdnd3Mtd2l6Il4oZGVzZW52b2x2ZWRvciB8IHByb2dyYW1hZG9yKSAoZnJvbnQtZW5kIHwgZnJvbnQgZW5kKSByZW1vdG8gY29udHJhdGFuZG8gfCBwcm9jdXJhbmRvIHwgYnJhc2lsSPiaAVDUlgFY1JYBcAB4AJABAJgBggKgAYICqgEDMi0xuAEDyAEA-AEC-AEBqAIA&sclient=gws-wiz&ibp=htl;jobs&sa=X&ved=2ahUKEwi4kMqYipCBAxWLiP0HHRnfD8gQudcGKAF6BAgTEBY#fpstate=tldetail&htivrt=jobs&htidocid=gtqo4D6IeF0AAAAAAAAAAA%3D%3D`;
+    const googleJobsUrl = `https://www.google.com.br/search?q=desenvolvedor+brasil&sca_esv=562432527&source=hp&ei=s1f1ZOWYOb6A9u8PlZGByAU&iflsig=AD69kcEAAAAAZPVlw3Ywn8w1JvWomVnny-qr_2Ars_vD&uact=5&oq=(desenvolvedor+%7C+programador)+(front-end+%7C+front+end)+remoto+contratando+%7C+procurando+%7C+brasil&gs_lp=Egdnd3Mtd2l6Il4oZGVzZW52b2x2ZWRvciB8IHByb2dyYW1hZG9yKSAoZnJvbnQtZW5kIHwgZnJvbnQgZW5kKSByZW1vdG8gY29udHJhdGFuZG8gfCBwcm9jdXJhbmRvIHwgYnJhc2lsSPiaAVDUlgFY1JYBcAB4AJABAJgBggKgAYICqgEDMi0xuAEDyAEA-AEC-AEBqAIA&sclient=gws-wiz&ibp=htl;jobs&sa=X&ved=2ahUKEwi4kMqYipCBAxWLiP0HHRnfD8gQudcGKAF6BAgTEBY#fpstate=tldetail&htivrt=jobs&htidocid=gtqo4D6IeF0AAAAAAAAAAA%3D%3D`;
     if (this.currentlyScrapingGoogle) return;
     console.log(googleJobsUrl)
     this.currentlyScrapingGoogle = true;
