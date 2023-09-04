b1576cee6eaba3d09a307abc2b7c2b8aa396e040
Douglas Santos <ds4208151@gmail.com>
2023-09-04 00:16:35 -0300

    fix

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const minutesToGoogleScraper = 10;
-const googleQuery = '(desenvolvedor | programador) (front-end | front end) remoto contratando | procurando'
+const googleQuery = '(desenvolvedor | programador) (front-end | front end) remoto contratando | procurando | brasil'
 
 module.exports = {
     minutesToGoogleScraper,
