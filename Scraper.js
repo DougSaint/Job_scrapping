90269634908a13ad05eb973a1994d25c2b73a863
Douglas Santos <ds4208151@gmail.com>
2023-09-04 01:00:09 -0300

    fix: size of screen

--- Scraper.js
+++ Scraper.js
@@ -28,7 +28,10 @@ class Scraper {
             : puppeteer.executablePath(),
       });
       const page = await browser.newPage();
-
+      await page.setViewport({
+        width: 1920,
+        height: 1080,
+      });
       await page.goto(googleJobsUrl);
       //make wait 10 seconds
 
